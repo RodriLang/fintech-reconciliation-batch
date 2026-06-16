9d308e52ae46d1b09fc9477a4306519f0087195d
EXC25890 <Xmt87p3.038z>
2026-06-16 16:47:44 -0300

    first commit

---
+++ src/main/java/com/portfolio/fintech_reconciliation_batch/FintechReconciliationBatchApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.fintech_reconciliation_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechReconciliationBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechReconciliationBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/fintech_reconciliation_batch/FintechReconciliationBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.fintech_reconciliation_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechReconciliationBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
